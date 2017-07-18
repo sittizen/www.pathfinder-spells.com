90d16c48ca33ac768bd65563c09314d22d756e3e
Simone Cittadini <simone@sig-c.com>
2017-07-18 09:51:18 +0200

    draft of rules implant.

--- pathfinder_spells/spells/models.py
+++ pathfinder_spells/spells/models.py
@@ -2,33 +2,57 @@ from django.core.validators import validate_comma_separated_integer_list
 from django.db import models
 
 
-MASTERIES_TYPES = {
+MASTERY_TYPES = {
+    0: 'School',
     1: 'Domain',
-    2: 'School',
-    3: 'Bloodline',
+    2: 'Bloodline',
+    3: 'Mystery',
+    4: 'Patron',
 }
 
 
 class Mastery(models.Model):
-    spell_type = models.PositiveIntegerField(choices=MASTERIES_TYPES.items())
+    """
+    A generic class to represent gerarchy of Schools, Domains, Bloodlines etc.
+    """
+    mastery_type = models.PositiveIntegerField(choices=MASTERY_TYPES.items(), default=0)
     name = models.CharField(max_length=128)
     specializes = models.ForeignKey('self', null=True, on_delete=models.CASCADE)
 
     class Meta:
-        unique_together = ('spell_type', 'name')
+        unique_together = ('mastery_type', 'name')
+
+    # todo checks on save, can't specialize a mastery of different type
 
     def __str__(self):
-        return u"%s: %s" % (MASTERIES_TYPES[self.spell_type],
+        return u"%s: %s" % (MASTERY_TYPES[self.mastery_type],
                             "%s (%s)" % (self.name, self.specializes.name) if self.specializes else self.name)
 
 
 class PlayerClass(models.Model):
     name = models.CharField(max_length=128, unique=True)
-    permitted_masteries_types = models.CharField(max_length=16, validators=(validate_comma_separated_integer_list, ))
-    masteries = models.ManyToManyField(Mastery)
+    must_prepare_spells = models.BooleanField(default=True)
+    permitted_mastery_types = models.CharField(max_length=16, default='0',
+                                               validators=(validate_comma_separated_integer_list, ))
 
     def __str__(self):
         return u"%s" % self.name
+
+    # todo this class will act as the main facade for queries
+    def spell_list(self, mastery_type=0, sub_masteries=[], level=None):
+
+        if mastery_type:
+            qs = MasteryLevel.objects.filter(mastery__in=Mastery.objects.filter(mastery_type__exact=mastery_type))
+            if sub_masteries:
+                qs = qs.filter(mastery__in=sub_masteries)
+        else:
+            qs = PlayerClassLevel.objects.filter(player_class__exact=self)
+            if sub_masteries:
+                qs = qs.filter(spell__masteries__in=sub_masteries)
+
+        if level is not None:
+            qs = qs.filter(level__exact=level)
+        return qs.order_by('level', 'spell__name').all()
 
 
 class Spell(models.Model):
@@ -37,15 +61,23 @@ class Spell(models.Model):
                                        through='MasteryLevel',
                                        through_fields=('spell', 'mastery'))
 
-    def add_to_mastery(self, mastery, level):
+    def add_to_mastery(self, mastery, level=None):
         MasteryLevel.objects.create(mastery=mastery, spell=self, level=level)
+
+    def add_to_playerclass(self, player_class, level=None):
+        PlayerClassLevel.objects.create(player_class=player_class, spell=self, level=level)
+
+    # todo custome create method checking that at least a School is associated with spell
 
     def __str__(self):
         return u"%s\n%s" % (self.name, "".join([" %s\n" % str(ml) for ml in self.masterylevel_set.all()]))
 
 
 class MasteryLevel(models.Model):
-    level = models.PositiveIntegerField()
+    """
+    Generate the list of spells by domain|bloodline|etc / level
+    """
+    level = models.PositiveIntegerField(null=True)  # positioning into School has no level
     spell = models.ForeignKey(Spell, on_delete=models.CASCADE)
     mastery = models.ForeignKey(Mastery, on_delete=models.CASCADE)
 
@@ -56,10 +88,38 @@ class MasteryLevel(models.Model):
         return u"%s, lvl %s" % (self.mastery, self.level)
 
 
+class PlayerClassLevel(models.Model):
+    """
+    Generate the list of spells by class / level
+    """
+    level = models.PositiveIntegerField()
+    spell = models.ForeignKey(Spell, on_delete=models.CASCADE)
+    player_class = models.ForeignKey(PlayerClass, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return u"%s %s %s" % (self.player_class.name, self.level, self.spell.name)
+
+
 class SpellsPerDay(models.Model):
     level = models.PositiveIntegerField()
-    num_of_spells = models.PositiveIntegerField()
     player_class = models.ForeignKey(PlayerClass)
+    mastery_type = models.PositiveIntegerField(default=0)  # eg clerics have separate spell per day for domain spells
+
+    num_of_spells = models.PositiveIntegerField()
+    apply_attribute_bonus = models.BooleanField(default=True)
 
     class Meta:
-        unique_together = ('level', 'num_of_spells', 'player_class')
+        unique_together = ('level', 'player_class', 'mastery_type')
+
+
+class SpellsKnown(models.Model):
+    level = models.PositiveIntegerField()
+    player_class = models.ForeignKey(PlayerClass)
+    mastery_type = models.PositiveIntegerField(default=0)
+    # todo absence of instance for 'lvl cls mst' axis means there is no cap. put rule in PlayerClass method
+
+    num_of_spells = models.PositiveIntegerField()
+    apply_attribute_bonus = models.BooleanField(default=True)
+
+    class Meta:
+        unique_together = ('level', 'player_class', 'mastery_type')
--- pathfinder_spells/spells/tests/test_models.py
+++ pathfinder_spells/spells/tests/test_models.py
@@ -1,29 +1,42 @@
 from test_plus.test import TestCase
 
-from pathfinder_spells.spells.models import (PlayerClass, Mastery, Spell, MasteryLevel, SpellsPerDay)
+from pathfinder_spells.spells.models import (PlayerClass, Mastery, Spell, SpellsPerDay)
 
 
 class TestSpells(TestCase):
-
-    def setUp(self):
+    def test_bard(self):
         pass
 
-    def test_use(self):
-        air_domain = Mastery.objects.create(spell_type=1, name='Air')
-        water_domain = Mastery.objects.create(spell_type=1, name='Water')
-        cloud_subdomain = Mastery.objects.create(spell_type=1, name='Cloud', specializes=air_domain)
+    def test_cleric(self):
+        cleric = PlayerClass.objects.create(name='Cleric', permitted_mastery_types='0,1')
+
+        conjuration_school = Mastery.objects.create(name='Conjuration')
+        summoning_school = Mastery.objects.create(name='Summoning', specializes=conjuration_school)
+
+        air_domain = Mastery.objects.create(mastery_type=1, name='Air')
+        cloud_subdomain = Mastery.objects.create(mastery_type=1, name='Cloud', specializes=air_domain)
+
+        water_domain = Mastery.objects.create(mastery_type=1, name='Water')
 
         obscuring_mist = Spell.objects.create(name='Obscuring Mist')
+        obscuring_mist.add_to_mastery(conjuration_school)
         obscuring_mist.add_to_mastery(air_domain, 1)
         obscuring_mist.add_to_mastery(water_domain, 1)
-        print(obscuring_mist)
+        obscuring_mist.add_to_playerclass(cleric, 1)
 
         solid_fog = Spell.objects.create(name='Solid Fog')
+        solid_fog.add_to_mastery(conjuration_school)
         solid_fog.add_to_mastery(cloud_subdomain, 4)
-        print(solid_fog)
 
-        cleric = PlayerClass.objects.create(name='Cleric', permitted_masteries_types='1')
+        abundant_ammunition = Spell.objects.create(name='Abundant Ammunition')
+        abundant_ammunition.add_to_mastery(summoning_school)
+        abundant_ammunition.add_to_playerclass(cleric, 1)
 
-        cleric.masteries.add(air_domain)
-        cleric.masteries.add(water_domain)
-        cleric.masteries.add(cloud_subdomain)
+        print(cleric.spell_list())
+        print(cleric.spell_list(0, [conjuration_school, ]))
+
+        print(cleric.spell_list(1))
+        print(cleric.spell_list(1, [water_domain, ]))
+        print(cleric.spell_list(1, [], 4))
+
+        print(cleric.spell_list(2))
